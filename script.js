db9f0ade68cad47946900ea0539a4c08e109f467
MarioAM96 <91484233+MarioAM96@users.noreply.github.com>
2023-04-03 20:34:46 -0500

    V1

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ function mostrarOcultarMenu(){
         menuVisible = true;
     }
 }
+
+function seleccionar(){
+    document.getElementById("nav").classList="";
+    menuVisible = flase;
+}
+ 
+
+//FUNCION PARA LAS ANIMACIONES DE SKILLS
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
